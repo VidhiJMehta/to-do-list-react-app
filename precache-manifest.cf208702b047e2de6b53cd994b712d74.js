43d9127505475dc922ce0eb687dc24cd3eb035a1
vidhijmehta <vidhimehta0524@gmail.com>
2020-07-04 18:56:52 -0500

    Updates

---
+++ precache-manifest.cf208702b047e2de6b53cd994b712d74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d88bb41767225f1aa46363972617780e",
+    "url": "/to-do-list-react-app/index.html"
+  },
+  {
+    "revision": "262b0f7add5a0ed387da",
+    "url": "/to-do-list-react-app/static/css/main.5a9a9e19.chunk.css"
+  },
+  {
+    "revision": "77a95e91e2f1923323cf",
+    "url": "/to-do-list-react-app/static/js/2.9c1fe7b0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/to-do-list-react-app/static/js/2.9c1fe7b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "262b0f7add5a0ed387da",
+    "url": "/to-do-list-react-app/static/js/main.f7c4a055.chunk.js"
+  },
+  {
+    "revision": "1681ef6b1906f9db1a2c",
+    "url": "/to-do-list-react-app/static/js/runtime-main.3cc8c3fc.js"
+  }
+]);
